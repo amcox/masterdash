533e0aeea1c3254ebb2c43fcd9b8f188218ba6b3
amcox <andrewmcox@gmail.com>
2015-10-23 16:30:03 -0500

    returned some import files

---
+++ sql/school_enrollments query.sql
@@ -0,0 +1,16 @@
+SELECT
+	s.student_number student_number,
+	e.entrydate entry_date,
+	e.exitdate exit_date,
+	sc.abbreviation school_abb,
+	e.grade_level grade,
+	ps_customfields.getStudentscf(s.id, 'la_sped') la_sped,
+	se.laa1 laa1,
+	t.name year
+FROM PSSIS_Enrollment_All e
+LEFT JOIN students s ON e.studentid = s.id
+LEFT JOIN schools sc ON e.schoolid = sc.school_number
+LEFT JOIN u_def_ext_students se ON se.studentsdcid = s.dcid
+LEFT JOIN terms t ON t.yearid = e.yearid AND t.portion = 1 AND t.schoolid = e.schoolid
+WHERE t.id LIKE '25%'
+AND e.grade_level < 99
