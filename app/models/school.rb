c294e1e9bb0466b3f4d2c1262b11250ab59c58f2
amcox <andrewmcox@gmail.com>
2015-10-23 16:25:58 -0500

    schools import method

--- app/models/school.rb
+++ app/models/school.rb
@@ -3,5 +3,16 @@ class School < ActiveRecord::Base
 	has_many :teachers, through: :teachings
 	has_many :enrollments
 	has_many :school_enrollments
-	
+  
+  def self.import(file_path)
+    require 'csv'
+    infile = File.read(file_path)
+    csv = CSV.parse(infile, {:headers => true, :header_converters => :symbol})
+    csv.each do |row|
+      school = School.create(name: row[:name], abbreviation: row[:abbreviation],
+        state_id: row[:state_id], street: row[:address]
+      )
+    end	
+  end
+  
 end
--- sql/students export.sql
+++ sql/students export.sql
@@ -9,6 +9,6 @@ FROM students s
 LEFT JOIN u_def_ext_students se ON se.studentsdcid = s.dcid
 JOIN schools sc ON sc.school_number = s.schoolid
 AND s.grade_level > -3
-AND s.schoolid IN (1, 2, 3, 5, 6, 7, 369701,369999)
+AND s.schoolid IN (1, 2, 3, 5, 6, 7, 369701, 369999)
 -- AND se.leap_count_student = 1
--- AND enroll_status = 0
+AND enroll_status = 0
